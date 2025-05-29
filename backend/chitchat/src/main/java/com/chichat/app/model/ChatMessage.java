360458fd03e8dbf62809127b4182158ea3f3049d
Sounak <sounaksaha333@gmail.com>
2025-05-30 02:58:15 +0530

    controller and model updation for chat messages

---
+++ backend/chitchat/src/main/java/com/chichat/app/model/ChatMessage.java
@@ -0,0 +1,12 @@
+package com.chichat.app.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatMessage {
+    private Long id;
+    private String sender;
+    private String content;
+}
