a51155c74779b0e6a92c5a27754c6bfc3c2d29ab
Sounak <sounaksaha333@gmail.com>
2025-05-30 02:33:16 +0530

    backend starter files

---
+++ backend/chitchat/src/test/java/com/chichat/chitchat/ChitchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chichat.chitchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChitchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
