c8130b2254d252f633d413c3dc0a06de513f5b52
Sounak <sounaksaha333@gmail.com>
2025-05-30 13:00:39 +0530

    thymeleaf template added

--- backend/chitchat/src/main/java/com/chichat/app/model/ChatMessage.java
+++ backend/chitchat/src/main/java/com/chichat/app/model/ChatMessage.java
@@ -1,10 +1,14 @@
 package com.chichat.app.model;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @NoArgsConstructor
+@Getter
+@Setter
 public class ChatMessage {
     private Long id;
     private String sender;
